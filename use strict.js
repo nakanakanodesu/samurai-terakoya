30b27893b7f4ec471b3cffe8062733c5d0097362
nakanakanodesu <145200296+nakanakanodesu@users.noreply.github.com>
2023-10-02 21:37:20 +0900

    Add files via upload

---
+++ use strict.js
@@ -0,0 +1,26 @@
+`use strict`;
+
+//1.
+function kur () {
+}
+
+//2.
+function kur (n) {
+}
+
+//3.
+function kur (n) {
+  return n;
+}
+
+/*4.
+できた関数をとりあえず呼んでみて、エラーが発生しないことを確認する
+*/
+
+//5.
+function kur (n) {
+  for(let i=0;i<n;i++){
+  return  * "*";
+}
+}
+console.log(kur(4));
