fed83a3cb458357b9542e72cddaf3cf4f807e8f9
nakanakanodesu <145200296+nakanakanodesu@users.noreply.github.com>
2023-10-09 18:59:29 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,26 +1,39 @@
-// ボタンの表示／非表示を切り替える関数
-const updateButton = () => {
-  if ($(window).scrollTop() >= 300) {
-    // 300px以上スクロールされた
-    // ボタンを表示
-    $(".back-to-top").fadeIn();
+//selectorに該当するタブを表示する関数
+
+const showTab = (selector) => {
+  // 引数selectorの中身をコンソールで確認する
+  console.log(selector);
+
+  /* 1. タブの選択状態のリセット */
+  // いったん、すべての.tabs-menuの子要素からactiveクラスを削除する
+  $(".tabs-menu > div").removeClass("active");
+
+  // ❌いったん、すべての.tabs-contentを非表示にする
+  // ❌$(".tabs-content").hide();
+  //tabs-contentの子要素を非表示にする
+  $(".tabs-content > div").hide();
+
+  /* 2. 選択されたタブの表示 */
+  // クリックされたselectorにactiveクラスを付けそれに対応したcontentを表示する
+  $(selector).addClass("active");
+  if (selector === `#tab-menu-a`) {
+    // ❌if (`#tab-menu-a="${selector}"`) {
+    console.log(selector);
+    $("#tabs-a").show();
+    // ❌} else if (`#tab-menu-b="${selector}"`) {
+  } else if (selector === `#tab-menu-b`) {
+    $("#tabs-b").show();
   } else {
-    // ボタンを非表示
-    $(".back-to-top").fadeOut();
+    $("#tabs-c").show();
   }
 };
 
-// スクロールされる度にupdateButtonを実行
-$(window).on("scroll", updateButton);
-
-// ボタンをクリックしたらページトップにスクロールする
-$(".back-to-top").on("click", (e) => {
-  // ボタンのhrefに遷移しない
-  e.preventDefault();
-
-  // 600ミリ秒かけてトップに戻る
-  $("html, body").animate({ scrollTop: 0 }, 600);
+// タブがクリックされたらコンテンツを表示
+$(".tabs-menu div").on("click", (e) => {
+  // idの値を受け取った後、showTab()関数に渡す。
+  const selector = $(e.target).attr("id");
+  showTab(`#` + selector);
 });
 
-// ページの途中でリロード（再読み込み）された場合でも、ボタンが表示されるようにする
-updateButton();
+// 初期状態として1番目のタブを表示
+showTab("#tab-menu-a");
